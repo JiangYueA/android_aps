659c6a9211b8a031a5c711e78262bc2309c80dc1
AllenJy <454976341@qq.com>
2016-12-29 09:49:17 +0800

    refresh

--- app/src/main/java/com/example/jiangyue/androidap/MenuActivity.java
+++ app/src/main/java/com/example/jiangyue/androidap/MenuActivity.java
@@ -1,11 +1,7 @@
 package com.example.jiangyue.androidap;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.TransitionDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -81,44 +77,44 @@ public class MenuActivity extends FragmentActivity implements OnListListener {
         tabs.setIndicatorColor(newColor);
 
         // change ActionBar color just if an ActionBar is available
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-
-            Drawable colorDrawable = new ColorDrawable(newColor);
-            Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
-            LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
-
-            if (oldBackground == null) {
-
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    ld.setCallback(drawableCallback);
-                } else {
-                    getActionBar().setBackgroundDrawable(ld);
-                }
-
-            } else {
-
-                TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
-
-                // workaround for broken ActionBarContainer drawable handling on
-                // pre-API 17 builds
-                // https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    td.setCallback(drawableCallback);
-                } else {
-                    getActionBar().setBackgroundDrawable(td);
-                }
-
-                td.startTransition(200);
-
-            }
-
-            oldBackground = ld;
-
-            // http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
-            getActionBar().setDisplayShowTitleEnabled(false);
-            getActionBar().setDisplayShowTitleEnabled(true);
-
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//
+//            Drawable colorDrawable = new ColorDrawable(newColor);
+//            Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
+//            LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+//
+//            if (oldBackground == null) {
+//
+//                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//                    ld.setCallback(drawableCallback);
+//                } else {
+//                    getActionBar().setBackgroundDrawable(ld);
+//                }
+//
+//            } else {
+//
+//                TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+//
+//                // workaround for broken ActionBarContainer drawable handling on
+//                // pre-API 17 builds
+//                // https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+//                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//                    td.setCallback(drawableCallback);
+//                } else {
+//                    getActionBar().setBackgroundDrawable(td);
+//                }
+//
+//                td.startTransition(200);
+//
+//            }
+//
+//            oldBackground = ld;
+//
+//            // http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+//            getActionBar().setDisplayShowTitleEnabled(false);
+//            getActionBar().setDisplayShowTitleEnabled(true);
+//
+//        }
 
         currentColor = newColor;
 
